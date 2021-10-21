368c02590135b01248991980ec216d537290cdc1
foxJ <backtothefuture3030@gmail.com>
2021-10-21 16:52:20 +0900

    10/21

--- 세븐포커프로그래밍.py
+++ 세븐포커프로그래밍.py
@@ -116,19 +116,65 @@ player_rank = 0
 
 
 # 플러쉬
-if com_pattern.count('♠︎')==5:
-    print("♠︎ flush")
-    com_rank+=30
-elif com_pattern.count('♦︎')==5:
-    print("♦︎ flush")
-    com_rank+=29
-elif com_pattern.count('♥︎')==5:
-    print("♥︎ flush")
-    com_rank+=28
-elif com_pattern.count('♣︎')==5:
-    print("♣︎ flush")
-    com_rank+=27
-else:
-    # 스트레이트 
+def flush(com_pattern):
+    if com_pattern.count('♠︎')==5:
+        print("com : ♠︎ flush")
+        return True
+    elif com_pattern.count('♦︎')==5:
+        print("com : ♦︎ flush")
+        return True
+    elif com_pattern.count('♥︎')==5:
+        print("com : ♥︎ flush")
+        return True
+    elif com_pattern.count('♣︎')==5:
+        print("com : ♣︎ flush")
+        return True
+    else:
+        return False
+
+# 스트레이트
+def straight(com_numbers):
+    com_numbers.sort(reverse=True)   
+    so = []
+    r = 0
+    if com_numbers[0]==13 and com_numbers[1]==12 and com_numbers[2]==11 and com_numbers[3]==10 and com_numbers[4]==9 and com_numbers[6]==1:
+        print("com : Mountain")
+        exit()
+    else:
+        for i in range(len(com_numbers)-1):
+            if com_numbers[i+1]-com_numbers[i]==-1:
+                if i==len(com_numbers)-2:
+                    so.append(com_numbers[i])
+                    so.append(com_numbers[i+1])
+                    r+=1
+                else:
+                    so.append(com_numbers[i])
+                    r+=1
+            elif r>=4:
+                so.append(com_numbers[i])
+                break
+            else:
+                r=0
+                so=[]
+        if r>=4:
+            if max(so)==5:
+                print("com : back straight")
+                return True
+            else:
+                print("com : {} straight".format(max(so)))
+                return True        # 랭크를 매겨야한다. 단순한 트루 뿐 아니라 점수까지 더해줘야한다. 
+        else:
+            del com_numbers[1]
+            del com_numbers[1]
+            if [1,10,11,12,13]==com_numbers:
+                print("com : Mountain")
+                return True
+            else:
+                #print("com의 최고 하이카드는 {}".format(max(com_numbers)))   # 스트레이트가 아님
+                return False
     
-    
+flush(com_pattern)
+straight(com_numbers)
+
+
+# 포카드, 트리플, 풀하우스, 투페어, 원페어 pair 판단하는 코드
