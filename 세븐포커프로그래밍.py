9baa2aaad18002191aa42ab78961ed7a0b36ab86
foxJ <backtothefuture3030@gmail.com>
2021-10-22 16:48:48 +0900

    10/22

--- 세븐포커프로그래밍.py
+++ 세븐포커프로그래밍.py
@@ -19,8 +19,8 @@ for k in range(3,6):
 
 com_num = []      # 카드들의 첫번째 장 레벨
 player_num = []
-print("com : ", end=' ')
-print(com)
+#print("com : ", end=' ')
+#print(com)
 print("player : " ,end=' ')
 print(player)
 
@@ -172,7 +172,51 @@ def straight(com_numbers):
             else:
                 #print("com의 최고 하이카드는 {}".format(max(com_numbers)))   # 스트레이트가 아님
                 return False
-    
+
+
+# 페어 
+def pair(com_numbers):
+    com_numbers.sort()
+    count = 0
+    pair = []
+    One_pair = False
+    Triple = False
+    Four_cards = False
+    for i in range(len(com_numbers)-1):
+        count = 0
+        if com_numbers[i]==com_numbers[i-1]:
+            pass
+        else:
+            for j in range(i+1,len(com_numbers)):
+                if com_numbers[i] == com_numbers[j]:
+                    count+=1
+            if count==1:
+                print("com : {} One pair".format(com_numbers[i]))
+                one = com_numbers[i]
+                pair.append(com_numbers[i])
+                One_pair = True
+                pair.append(com_numbers[i])
+            elif count==2:
+                print("com : {} Triple".format(com_numbers[i]))
+                triple = com_numbers[i]
+                Triple = True
+            elif count==3:
+                print("com : {} Four cards".format(com_numbers[i]))
+                Four_cards = True
+    pair = list(set(pair))
+    if Four_cards == True:                                   # 포카드
+        pass
+    elif One_pair == True and Triple == True:                # 풀하우스
+        print("com : {},{} Full House".format(one,triple))
+    elif One_pair == True and len(list(set(pair)))>=2:       # 투페어
+        print("com : {},{} Two Pair".format(pair[-2],pair[-1]))
+    elif One_pair == False and Triple == False and Four_cards == False:
+        print("com : {} card".format(max(com_numbers)))
+
+
+
+
+pair(com_numbers)
 flush(com_pattern)
 straight(com_numbers)
 
