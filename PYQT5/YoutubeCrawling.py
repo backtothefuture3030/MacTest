c12eb5f0b30fa42598ed5dde344ac19004c9b182
foxJ <backtothefuture3030@gmail.com>
2021-11-17 15:59:55 +0900

    selenium+bs4

--- PYQT5/YoutubeCrawling.py
+++ PYQT5/YoutubeCrawling.py
@@ -1,41 +1,69 @@
-'''
-import selenium
+# selenium , bs4 혼합 사용시 빠르고 간편하다.
+
+from selenium.webdriver import Chrome
 from selenium import webdriver
-from time import sleep
 import time
-
-class YoutubeCrawler():
-    url = 'https://www.youtube.com/'
-    options = webdriver.ChromeOptions()
-    options.add_argument("headless")
-
-    titles = []
-    href = []
-
-    driver = webdriver.Chrome(executable_path='chromedriver', options=options)
-    driver.get(url)
-    for i in range(2,7):
-        title = driver.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-rich-grid-renderer/div[7]/ytd-rich-item-renderer[{}]/div/ytd-rich-grid-media/div[1]/div[2]/div[1]/h3/a'.format(i))
-        titles.append(title.get_attribute('title'))
-        href.append(title.get_attribute('href'))
-    print(titles)
-    print(href)
-
-    driver.quit()
-
-YoutubeCrawler()
-'''
-# selenium 을 이용하니까 pyqt 작동이 너무느리고 안통한다 bs4 를 사용하는게..
-
 from bs4 import BeautifulSoup
-import requests
+from urllib import parse
 import sys
-from PyQt5.QtWidgets import *
-from PyQt5 import uic
-from PyQt5.QtCore import QDate
+from PyQt5 import QtCore, QtWidgets, uic
+from PyQt5.QtWidgets import QDial, QDialog, QMainWindow
 import re
 
 form_class = uic.loadUiType('/Users/sup/Desktop/Mycode/PYQT5/Youtubeb.ui')[0]
 
-class MyWindow(QDialog, form_class):
+class Mywindow(QDialog, form_class):  # 윈도우 만들어주기, 조작 정의
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        # 버튼 기능 정의
+        self.crawlStart.clicked.connect(self.clicked_crawlStart)  # [버튼] 크롤링 시작
+        #self.articleTable.clicked.connect(self.clicked_articleTable) articleTable 열을 눌렀을때 url 연결 구현 (개발중)
+    '''
+    def clicked_articleTable(self): # # [버튼] articleTable의 링크 열을 눌렀을 때, 해당 링크로 연결
+        if item.column() == 1:
+            webbrowser.open('www.google.com')'''
+    
+    def Crawling():
+
+
+options = webdriver.ChromeOptions()
+options.add_argument("headless")
+
+url = 'https://www.youtube.com/'
+driver = Chrome(options=options)
+driver.get(url)
+
+scroll_pane_height = driver.execute_script('return document.documentElement.scrollHeight')  # 스크롤 바 내리기
+
+while True:
+    # 스크롤바를 스크롤패인 높이 만큼 이동
+    driver.execute_script('window.scrollTo(0,document.documentElement.scrollHeight)')
+    # 밑에 붙일 내용을 요청해서 시간차를 둔다
+    time.sleep(1)
+    new_scroll_pane_height = driver.execute_script('return document.documentElement.scrollHeight')
+    #print(scroll_pane_height, new_scroll_pane_height)
+    if scroll_pane_height == new_scroll_pane_height:
+        break
+    scroll_pane_height = new_scroll_pane_height
+
+
+soup = BeautifulSoup(driver.page_source)
+a_list = soup.select('div#contents div#content a#video-title-link')
+titles = []
+links = [] 
+for a_tag in a_list:
+    link = parse.urljoin(url, a_tag.get('href'))  # url 의 상대경로를 절대경로로 바꾼다.
+    title = a_tag.text
+    titles.append(title)
+    links.append(link)
+
+for i in range(0,len(titles)):
+    print(titles[i] + "  :  " + links[i])
+
+
+
+
+
 
